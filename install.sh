de71ee9c1dfdeab56dccc70c0bb2e6ec2e192329
quietc4t <51285014+quietc4t@users.noreply.github.com>
2019-06-04 16:18:24 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,48 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+
+toilet -f ivrit 'QUIET CAT' | boxes -d cat -a hc -p h8 | lolcat
+sleep 1
+echo '''\a
+\033[32;1m                                          __                          
+\033[32;1m          /           /         / /      /              /             
+\033[32;1m         (  ___  ___ (___  ___ ( (      (___       ___ (___  ___  _ _ 
+\033[32;1m         | |   )|___ |    |   )| |          )\   )|___ |    |___)| | )
+\033[32;1m         | |  /  __/ |__  |__/|| |       __/  \_/  __/ |__  |__  |  / 
+                                               /                      '''
+sleep 3
+apt update && apt upgrade
+apt install nano
+apt install git
+pkg install python python2 vim figlet curl
+clear
+echo '''\a
+\033[37;1m                                        __                               
+\033[37;1m        /           /         / /      /  |           /                  
+\033[37;1m       (  ___  ___ (___  ___ ( (      (___| ___  ___ (     ___  ___  ___ 
+\033[37;1m       | |   )|___ |    |   )| |      |    |   )|    |___)|   )|   )|___)
+\033[37;1m       | |  /  __/ |__  |__/|| |      |    |__/||__  | \  |__/||__/ |__  
+\033[37;1m                                                               __/       '''
+sleep 3
+apt install toilet
+apt install boxes
+apt install php
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+clear
+echo '''\a
+\033[37;1m                  ___                                           
+\033[37;1m                 /    /      /      /    /                      
+\033[37;1m                (___    ___    ___ (___    ___  ___             
+\033[37;1m                |    | |   )| |___ |   )| |   )|   )            
+\033[37;1m                |    | |  / |  __/ |  / | |  / |__/             
+\033[37;1m                                               __/  -   -   -  '''
+sleep 4
+sh quietcat.sh
---
+++ quietcat.sh
@@ -0,0 +1,297 @@
+#!/bin/bash
+
+# easysploit
+#===============================================#
+# [X] AUTHOR 	: QUIETC4T                 	#
+# [X] CODED BY  : QUIETC4T		   	#
+# [X] EMAIL	: quietc4t@gmail.com	   	#
+# [X] GITHUB	: https://github.com/quietc4t/	#
+#===============================================#
+# TOLONG DONG JANGAN DI RECODE .. !!		#
+# CAPE BANGSAT GUA NGODING NYE ..!!		#
+# SAMPE SAMPE TULANG PUNGGUNG+LEHER PEGELLL..!!	#
+# KEBAYANG PEGELNYA SAMPE DNA ..!!		#
+# MAKA DARI ITU YA GUYS TOLONG JANGAN DI RECODE	#
+# BOLEH DI RECODE ASAL CANTUMIN AUTHON		#
+# MOHON KERJA SAMANYA				#
+# TERIMA KASIH SUDAH TIDAK MERECODE INI ^_^..	#
+#===============================================#
+# SAYA BERJANJI TIDAK AKAN RECODE INI ^_^
+#===============================================#
+
+#user/bin/bash
+#!/bin/bash
+
+# quietcat
+clear
+#============ INSTALL COLOR ===============#
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+#==========================================#
+
+#============== MENU =================#
+toilet -f ivrit 'QUIET CAT' | boxes -d cat -a hc -p h8 | lolcat
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$cy"     	 QUIET CAT Facebook Tools "$i"             |"
+echo $i" |"$me"─────────────────────────────────────────────"$i"|"
+echo $i" |"$pu" AUTHOR :"$ku" QUIETCAT"$i"                           |"
+echo $i" |"$pu" GITHUB :"$ku" https://github.com//quietc4t"$i"       |"
+echo $i" |"$pu" GMAIL :"$ku" quietc4t@gmail.com"$i"                  |"
+echo $i" |"$pu" YOUTUBE :"$ku" -"$i"                                 |"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo
+echo $i"["$bi"•"$i"]"$me"─"$bi"["$i"Menu Tools"$bi"]"$me"────────────────────────"$i"["$bi"•"$i"]"
+echo
+echo $bi"────────────────────────────────"
+echo $bi"|"$me" 1"$bi" |"$i" Report Facebook          "$bi"|"$bi""
+echo $bi"|"$me" 2"$bi" |"$i" Brute Force              "$bi"|"$bi""
+echo $bi"|"$me" 3"$bi" |"$i" Brute Force Facebook     "$bi"|"$bi""
+echo $bi"|"$me" 4"$bi" |"$i" MBF                      "$bi"|"$bi""
+echo $bi"|"$me" 5"$bi" |"$i" Yahoo Cloning            "$bi"|"$bi""
+echo $bi"|"$me" 6"$bi" |"$i" OSIF                     "$bi"|"$bi""
+echo $bi"────────────────────────────────"$bi""
+echo $bi"|"$me" 7"$bi" |"$i" Auto Reaction Facebook   "$bi"|"$bi""
+echo $bi"|"$me" 8 "$bi"|"$i" Profile Guard Facebook   "$bi"|"$bi""
+echo $bi"|"$me" 9"$bi" |"$i" Information Facebook     "$bi"|"$bi""
+echo $bi"|"$me"10 "$bi"|"$i" Bot Komen Facebook       "$bi"|"$bi""
+echo $bi"|"$me"11 "$bi"|"$i" Auto Likes Facebook      "$bi"|"$bi""
+echo $bi"────────────────────────────────"$bi""
+echo $bi"|"$me"12 "$bi"|"$i" Phising Facebook 1       "$bi"|"$bi""
+echo $bi"|"$me"13 "$bi"|"$i" Phising Facebook 2       "$bi"|"$bi""
+echo $bi"|"$me"14 "$bi"|"$i" Phising Facebook 3       "$bi"|"$bi""
+echo $bi"|"$me"15 "$bi"|"$i" Phising Facebook 4       "$bi"|"$bi""
+echo $bi"────────────────────────────────"$bi""
+echo $bi"|"$me"16 "$bi"|"$i" Profile Guard Facebook 2 "$bi"|"$bi""
+echo $bi"|"$me"17 "$bi"|"$i" YouTube Convert Mp3      "$bi"|"$bi""
+echo $bi"|"$me"18 "$bi"|"$i" Jadwal Shalat            "$bi"|"$bi""
+echo $bi"|"$me"19 "$bi"|"$i" Evil Droid               "$bi"|"$bi""
+echo $bi"|"$me"19 "$bi"|"$i" Easy Sploit              "$bi"|"$bi""
+echo $bi"────────────────────────────────"
+echo $bi"|"$me" 0"$bi" |"$i" Exit                     "$bi"|"$bi""
+echo $bi"────────────────────────────────"
+echo
+echo $me"┌==="$bi"["$i"QUIETC4T"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/Senitopeng/fbbrute.git
+cd fbbrute
+python2 jomblo.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3.git
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook.git
+cd multi-bruteforce-facebook
+python2 MBF.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/N74NK/Yclone.git
+cd Yclone
+pip2 install -r requirements.txt
+python2 yclone.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/AMVengeance/FB-React
+cd FB-React
+./start
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/hatakecnk/FB-Shield
+cd ProfileGuardFb
+php guard.php
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/FR13ND8/InfoFB
+cd InfoFB
+pip2 install -r requirements.txt
+python2 info.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/FR13ND8/autolike
+cd autolike
+php autolike.php
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/thelinuxchoice/blackeye.git
+cd blackeye
+bash blackeye.sh
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/evait-security/weeman.git
+chmod +x *
+python2 weeman.py
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "T U G G U"|lolcal
+sleep 2
+git clone https://github.com/hatakecnk/FB-Shield
+cd FB-Shield
+python2 fb-shield.py
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/hatakecnk/Convert-Mp3
+cd Convert-Mp3
+python2 converter.py
+fi
+
+if [ $pil = 18 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/hatakecnk/JadwalShalat
+cd JadwalShalat
+python2 jadwalshalat.py
+fi
+
+if [ $pil = 19 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/M4sc3r4n0/Evil-Droid
+cd Evil-Droid
+bash evil-droid
+fi
+
+if [ $pil = 20 ]
+then
+clear
+figlet -f slant "T U N G G U"|lolcat
+sleep 2
+git clone https://github.com/quietc4t/easysploit.git
+cd easysploit
+sh install.sh
+fi
+
+if [ $pil = 0 ]
+then
+clear
+sleep 2
+figlet -f slant "GOOD BYE NGNTD"|lolcat
+sleep 1
+figlet -f slant "JANGAN LUPA COLI"|lolcat
+exit
+fi
